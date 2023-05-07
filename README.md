8c11194c7d2aa18033704d36dd74fbde4bff2d64
Vincent Vu <vinceny.fe@gmail.com>
2023-05-06 23:43:57 -0700

    make it a CLI + add readme usage

--- README.md
+++ README.md
@@ -1,8 +1,42 @@
 # curconv
 
-A very simple currency converter.
+A very simple currency converter CLI.
 
 I heard somewhere that Google asked this for their phone screen; actually neat application of graph traversal. You could think of this as that currency
 converter on Google that lets you choose any pair and get the rate for it.
 
 Written with Go because why not; it's good at computation.
+
+### Usage
+
+```
+NAME:
+   curconv - A currency converter CLI tool that can find the rate between any two pairs in your given currency list
+
+USAGE:
+   ./curconv [global options] <currency from> <currency to> [args]
+
+COMMANDS:
+   help, h  Shows a list of commands or help for one command
+
+GLOBAL OPTIONS:
+   --verbose, -v           Do it verbosely (default: false)
+   --file value, -f value  Path to CSV file containing comma-delimited currency pairs
+   --help, -h              show help
+
+EXAMPLE:
+$ cat currencies.csv
+USD,CAD,1.35
+CHF,CAD,1.53
+
+$ ./curconv -f currencies.csv -v CHF USD
+2023/05/06 23:42:22 Created adjacency list of currency pairs: map[CAD:[{USD 0.7407407407407407} {CHF 0.6535947712418301}] CHF:[{CAD 1.53}] USD:[{CAD 1.35}]]
+2023/05/06 23:42:22 Done traversing currency pairs; successor table: map[CAD:{USD 0.7407407407407407} CHF:{CAD 1.53}]
+Steps:
+1 CHF = 1.530000 CAD
+1 CAD = 0.740741 USD
+--------------
+1 CHF = 1.133333 USD
+
+
+```
--- main.go
+++ main.go
@@ -1,97 +1,198 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"strconv"
+	"strings"
+
+	"github.com/urfave/cli/v2"
 )
 
 type AdjListNode struct {
-  endpoint string
-  value float64
+	endpoint string
+	value    float64
 }
 
 type AdjList map[string][]AdjListNode
 
 type CurrencyPair struct {
-  left string
-  right string
-  value float64
+	left  string
+	right string
+	value float64
 }
 
-func findExchangeRate(left, right string, adjList AdjList) (float64, error) {
-  if _, ok := adjList[left]; !ok {
-    return -1, fmt.Errorf("No connecting currency pair provided for %s", left)
-  }
-  if _, ok := adjList[right]; !ok {
-    return -1, fmt.Errorf("No connecting currency pair provided for %s", right)
-  }
-  fwdtable := map[string]AdjListNode{}
-  visited := map[string]bool{}
-  stack := []string{}
-  stack = append(stack, left)
-  for len(stack)!=0 {
-    top := stack[len(stack)-1]
-    stack = stack[:len(stack)-1]
+func findExchangeRate(left, right string, adjList AdjList, showStepByStep bool) (float64, *[]CurrencyPair, error) {
+	if _, ok := adjList[left]; !ok {
+		return -1, nil, fmt.Errorf("No connecting currency pair provided for %s", left)
+	}
+	if _, ok := adjList[right]; !ok {
+		return -1, nil, fmt.Errorf("No connecting currency pair provided for %s", right)
+	}
 
-    if top == right {
-      break
-    }
+	// DFS down the currency graph
+	fwdtable := map[string]AdjListNode{}
+	visited := map[string]bool{}
+	stack := []string{}
+	stack = append(stack, left)
+	for len(stack) != 0 {
+		top := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
 
-    if vsted, ok := visited[top]; !ok || !vsted {
-      visited[top] = true
-      for _, neighbor := range adjList[top] {
-        stack = append(stack, neighbor.endpoint)
-        fwdtable[top] = AdjListNode{neighbor.endpoint, neighbor.value}
-      }
-    }
-  }
+		if vsted, ok := visited[top]; !ok || !vsted {
+			visited[top] = true
+			for _, neighbor := range adjList[top] {
+				if visited[neighbor.endpoint] {
+					continue
+				}
+				stack = append(stack, neighbor.endpoint)
+				fwdtable[top] = AdjListNode{neighbor.endpoint, neighbor.value}
+			}
+		}
+	}
 
+	if showStepByStep {
+		log.Printf("Done traversing currency pairs; successor table: %v\n", fwdtable)
+	}
 
-  excRate := 1.0
-  for {
-    nexthop := fwdtable[left]
-    excRate = excRate*nexthop.value
-    if nexthop.endpoint == right {
-      break
-    }
-    left = nexthop.endpoint
-  }
+	// trace back from left -> right via predecessors
+	excRate := 1.0
+	interconvs := []CurrencyPair{}
+	for {
+		nexthop := fwdtable[left]
+		excRate = excRate * nexthop.value
+		if showStepByStep {
+			interconvs = append(interconvs, CurrencyPair{left, nexthop.endpoint, nexthop.value})
+		}
+		if nexthop.endpoint == right {
+			break
+		}
+		left = nexthop.endpoint
+	}
 
-  return excRate, nil
+	if showStepByStep {
+		return excRate, &interconvs, nil
+	}
+
+	return excRate, nil, nil
+}
+
+// construct adjacency list from the currency pair
+func constructAdjListFromCurrencyPairs(pairs []CurrencyPair) AdjList {
+	adjList := AdjList{}
+	for _, p := range pairs {
+		if _, ok := adjList[p.left]; !ok {
+			adjList[p.left] = []AdjListNode{}
+		}
+		adjList[p.left] = append(adjList[p.left], AdjListNode{
+			endpoint: p.right,
+			value:    p.value,
+		})
+		if _, ok := adjList[p.right]; !ok {
+			adjList[p.right] = []AdjListNode{}
+		}
+		adjList[p.right] = append(adjList[p.right], AdjListNode{
+			endpoint: p.left,
+			value:    1 / p.value,
+		})
+	}
+	return adjList
+}
+
+func readCurrenciesCSV(path string) (*[]CurrencyPair, error) {
+	pairs := []CurrencyPair{}
+
+	f, err := os.Open(path)
+	defer f.Close()
+	reader := bufio.NewScanner(f)
+	reader.Split(bufio.ScanLines)
+
+	linenum := 1
+	for reader.Scan() {
+		line := reader.Text()
+		if err != nil {
+			break
+		}
+		linenum++
+		splitted := strings.Split(line, ",")
+		if len(splitted) != 3 {
+			return nil, fmt.Errorf("Line %d is invalid; must have exactly 3 values between commas; got '%s'\n", linenum, line)
+		}
+		left := strings.Trim(splitted[0], " ")
+		right := strings.Trim(splitted[1], " ")
+		value, err := strconv.ParseFloat(splitted[2], 64)
+		if err != nil {
+			return nil, fmt.Errorf("Line %d is invalid; value not an integer; got '%s'\n", linenum, splitted[2])
+		}
+		pairs = append(pairs, CurrencyPair{left, right, value})
+	}
+	return &pairs, nil
 }
 
 func main() {
-  pairs := []CurrencyPair{
-    {"USD", "CAD", 1.35},
-    {"CHF", "CAD", 1.53},
-  }
+	app := &cli.App{
+		Name:    "curconv",
+		Usage:   "A currency converter CLI tool that can find the rate between any two pairs in your given currency list",
+		Suggest: true,
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:    "verbose",
+				Aliases: []string{"v"},
+				Usage:   "Do it verbosely",
+			},
+			&cli.StringFlag{
+				Name:     "file",
+				Aliases:  []string{"f"},
+				Usage:    "Path to CSV file containing comma-delimited currency pairs",
+				Required: true,
+			},
+		},
+		UsageText: "./curconv [global options] <currency from> <currency to> [args]",
+		Action: func(ctx *cli.Context) error {
+			left := ctx.Args().Get(0)
+			right := ctx.Args().Get(1)
+			verbose := ctx.Bool("v")
 
-  adjList := AdjList{}
-  for _, p := range pairs {
-    if _, ok := adjList[p.left]; !ok {
-      adjList[p.left] = []AdjListNode{}
-    }
-    adjList[p.left] = append(adjList[p.left], AdjListNode{
-      endpoint: p.right, 
-      value: p.value,
-    })
-    if _, ok := adjList[p.right]; !ok {
-      adjList[p.right] = []AdjListNode{}
-    }
-    adjList[p.right] = append(adjList[p.right], AdjListNode{
-      endpoint: p.left,
-      value: 1/p.value,
-    });
-  }
+			pairs, err := readCurrenciesCSV(ctx.String("file"))
+			if err != nil {
+				return err
+			}
 
-  fmt.Println(adjList)
-  
-  left := "USD"
-  right := "CHF"
-  excRate, err := findExchangeRate(left, right, adjList)
-  if err !=nil {
-    fmt.Println(err.Error())
-    os.Exit(1)
-  }
-  fmt.Printf("1 %s = %f %s\n", left, excRate, right)
+			adjList := constructAdjListFromCurrencyPairs(*pairs)
+
+			if verbose {
+				log.Printf("Created adjacency list of currency pairs: %v\n", adjList)
+			}
+
+			if left == "" {
+				return fmt.Errorf("Must provide the currency to convert from")
+			}
+			if right == "" {
+				return fmt.Errorf("Must provide the currency to convert to")
+			}
+			excRate, interconvs, err := findExchangeRate(left, right, adjList, verbose)
+			if err != nil {
+				fmt.Println(err.Error())
+				os.Exit(1)
+			}
+
+			if verbose {
+				fmt.Printf("Steps:\n")
+				for _, pair := range *interconvs {
+					fmt.Printf("1 %s = %f %s\n", pair.left, pair.value, pair.right)
+				}
+				fmt.Printf("--------------\n")
+			}
+
+			fmt.Printf("1 %s = %f %s\n", left, excRate, right)
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+
 }
